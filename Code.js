6d32d8cebf1c780ae54d7b009e75e1fe8d6f38c9
osvaldom <mekymaria@gmail.com>
2020-11-25 16:54:33 +0200

    Create html and text template files

--- Code.js
+++ Code.js
@@ -1,3 +1,6 @@
+//ideas from: https://spreadsheet.dev/send-html-email-from-google-sheets
+
+
 // This constant is written in column C for rows for which an email
 // has been sent successfully.
 var EMAIL_SENT = 'EMAIL_SENT';
@@ -5,25 +8,49 @@ var EMAIL_SENT = 'EMAIL_SENT';
 /**
  * Sends non-duplicate emails with data from the current spreadsheet.
  */
-function sendEmails() {
+function sendEmails(){
   var sheet = SpreadsheetApp.getActiveSheet();
   var startRow = 2; // First row of data to process
+  var startCol = 1;
   var numRows = 290; // Number of rows to process
-  // Fetch the range of cells A2:B3
-  var dataRange = sheet.getRange(startRow, 1, numRows, 1);
+  var numCols = 4;
+  var dataRange = sheet.getDataRange(); //use sheet.getRange(startRow, startCol, numRows, numCols) to retrieve specific range of rows/columns;
+
   // Fetch values for each row in the Range.
   var data = dataRange.getValues();
-  for (var i = 0; i < data.length; ++i) {
+  for(var i = 0; i < data.length; ++i){
     var row = data[i];
-    var emailAddress = row[0]; // First column
-    var htmlMessage = `<p dir="ltr">Caro cliente,</p><p dir="ltr">A equipa emprego.co.mz vem atrav&eacute;s desta nota pedir desculpas pelo envio repetitivo de e-mails, no passado dia 06 de Fevereiro corrente.</p><p dir="ltr">Na expectativa de apresentar aos utilizadores as novas funcionalidades da plataforma, registou-se um erro no sistema de e-mail, que causou o envio de m&uacute;ltiplas mensagens para o mesmo remetente.</p><p dir="ltr">Pelos transtornos causados, a equipa emprego.co.mz pede sinceras desculpas.</p><p dir="ltr">&nbsp;&nbsp;</p><p dir="ltr">Cumprimentos,</p><p dir="ltr">A equipa emprego.co.mz</p>';`
-    var emailSent = row[1]; // Second column
-    if (emailSent !== EMAIL_SENT) { // Prevents sending duplicates
-      var subject = 'emprego.co.mz: erro no envio de e-mail';
-      MailApp.sendEmail({ito: emailAddress, subject: subject, htmlBody: htmlMessage});
-      sheet.getRange(startRow + i, 2).setValue(EMAIL_SENT);
+    var emailAddress = row[1]; // First column
+    var link = row[3];
+    var htmlMessage = getEmailHtml(link);
+    var txtMessage = getEmailText(link);
+    var emailSent = row[4]; // 4th column
+    if(emailAddress && (emailSent !== EMAIL_SENT)){ // Prevents sending duplicates
+      var subject = 'Actualização dos contactos dos clientes';
+      MailApp.sendEmail({
+        to: emailAddress,
+        subject: subject,
+        body: txtMessage,
+        htmlBody: htmlMessage
+      });
+      sheet.getRange(startRow + i, 5).setValue(EMAIL_SENT); //mark the sent row with email sent
       // Make sure the cell is updated right away in case the script is interrupted
       SpreadsheetApp.flush();
     }
   }
+}
+
+
+function getEmailHtml(data){
+  var htmlTemplate = HtmlService.createTemplateFromFile("Template.html");
+  htmlTemplate.data = data;
+  var htmlBody = htmlTemplate.evaluate().getContent();
+  return htmlBody;
+}
+
+function getEmailText(data){
+  var htmlTemplate = HtmlService.createTemplateFromFile("Template.txt");
+  htmlTemplate.data = data;
+  var htmlBody = htmlTemplate.evaluate().getContent();
+  return htmlBody;
 }
